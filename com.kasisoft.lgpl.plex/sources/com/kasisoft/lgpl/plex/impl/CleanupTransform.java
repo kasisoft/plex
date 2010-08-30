1fa761299b2fd57438ee793a5288c5e08a242428
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-08-30 22:17:23 +0000

    + initial api implementations

---
+++ com.kasisoft.lgpl.plex/sources/com/kasisoft/lgpl/plex/impl/CleanupTransform.java
@@ -0,0 +1,31 @@
+/**
+ * Name........: CleanupTransform
+ * Description.: Cleans up a string which means that it will be trimmed and useless values will be
+ *               corrected.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.plex.impl;
+
+import com.kasisoft.lgpl.plex.api.*;
+
+/**
+ * Cleans up a string which means that it will be trimmed and useless values will be corrected.
+ * All non-String types will remain untouched.
+ */
+public class CleanupTransform implements ValueTransform {
+
+  /**
+   * {@inheritDoc}
+   */
+  public Object transformValue( Object value, String ... args ) {
+    if( value instanceof String ) {
+      return ((String) value).trim();
+    } else {
+      return value;
+    }
+  }
+
+} /* ENDCLASS */
