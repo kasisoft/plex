77091510a92719a0daafdc3b433005ed98c80c34
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-08-31 21:15:12 +0000

    + a simple resolver used to identify a first row

---
+++ com.kasisoft.lgpl.plex/sources/com/kasisoft/lgpl/plex/impl/SimpleRowResolver.java
@@ -0,0 +1,40 @@
+/**
+ * Name........: SimpleRowResolver
+ * Description.: This RowResolver implementation simply checks for the occurrence of the first
+ *               row which contains content.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package com.kasisoft.lgpl.plex.impl;
+
+import com.kasisoft.lgpl.plex.*;
+import com.kasisoft.lgpl.plex.api.*;
+
+import org.apache.poi.ss.usermodel.*;
+
+/**
+ * This RowResolver implementation simply checks for the occurrence of the first row which contains 
+ * content.
+ */
+public class SimpleRowResolver implements RowResolver {
+
+  private static final String SYNTAX  = "SimpleRowResolver(int)";
+  
+  /**
+   * {@inheritDoc}
+   */
+  public int detectRow( Sheet sheet, String... args ) throws PLEXException {
+    int offset = 0;
+    if( args.length > 0 ) {
+      try {
+        offset = Integer.parseInt( args[0] );
+      } catch( NumberFormatException ex ) {
+        throw new PLEXException( PLEXFailure.InvalidApiCall, "", SYNTAX );
+      }
+    }
+    return sheet.getFirstRowNum() + offset;
+  }
+
+} /* ENDCLASS */
