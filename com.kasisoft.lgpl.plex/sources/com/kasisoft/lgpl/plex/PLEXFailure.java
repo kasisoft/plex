80cfd5eb4cfb8e574b75b9b83b764d2d0c9e7c35
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-09-02 21:03:03 +0000

    - integrated the functionality from 'PLEXUtilities' into 'Importer'

--- com.kasisoft.lgpl.plex/sources/com/kasisoft/lgpl/plex/PLEXFailure.java
+++ com.kasisoft.lgpl.plex/sources/com/kasisoft/lgpl/plex/PLEXFailure.java
@@ -14,7 +14,6 @@ package com.kasisoft.lgpl.plex;
 public enum PLEXFailure {
 
   ErrorInApiFunction  ( "An error occured within an API function ! Cause: %s" ),
-  InvalidApiCall      ( "ID: %s accepts the following args: %s !"             ),
   IO                  ( "The resource '%s' could not be loaded !"             ),
   InvalidExcel        ( "The excel file '%s' is not valid !"                  ),
   DeclarationError    ( "%s"                                                  ),
