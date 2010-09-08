11f767ba1fe6be1ed95abff19bbce8ef0a3f0f2b
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-09-08 22:03:52 +0000

    + initial provision of examples

---
+++ com.kasisoft.lgpl.libs.plex.examples/sources/example/RunImport.java
@@ -0,0 +1,54 @@
+/**
+ * Name........: RunImport
+ * Description.: Sample application used for the import.
+ * Author......: Daniel Kasmeroglu
+ * E-Mail......: daniel.kasmeroglu@kasisoft.net
+ * Company.....: Kasisoft
+ * License.....: LGPL
+ */
+package example;
+
+import com.kasisoft.lgpl.libs.plex.instance.*;
+import com.kasisoft.lgpl.libs.plex.*;
+
+import java.net.*;
+import java.io.*;
+
+/**
+ * Sample application used for the import.
+ */
+public class RunImport {
+
+  public static final void main( String[] args ) {
+
+    if( args.length == 0 ) {
+      System.err.println( "Missing argument !" );
+      return;
+    }
+    
+    String   name        = args[0];
+    File     declaration = new File( String.format( "%s.plex", name ) );
+    Importer importer    = null;
+    try {
+      importer = new Importer( declaration.toURI().toURL() );
+    } catch( MalformedURLException ex ) {
+      // we've made sure that the resource and thus the URL is correct, so ignore this
+      return;
+    } catch( PLEXException ex ) {
+      // the declaration file was obviously invalid
+      ex.printStackTrace();
+      return;
+    }
+    
+    try {
+      File       excel    = new File( String.format( "%s.xls", name ) );
+      PlainExcel plex     = importer.runImport( excel );
+      System.out.println( plex );
+    } catch( PLEXException ex ) {
+      // the import failed for some reason
+      ex.printStackTrace();
+    }
+
+  }
+  
+} /* ENDCLASS */
