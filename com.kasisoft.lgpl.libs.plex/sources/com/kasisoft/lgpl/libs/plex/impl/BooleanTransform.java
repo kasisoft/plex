0c22894ae6158a670d26ba3bbf4d53c7d0596c9c
Daniel Kasmeroglu <daniel.kasmeroglu@kasisoft.net>
2010-09-26 20:37:17 +0000

    + added the method 'setTrue' which allows to configure the values that are supposed to be identified as true

--- com.kasisoft.lgpl.libs.plex/sources/com/kasisoft/lgpl/libs/plex/impl/BooleanTransform.java
+++ com.kasisoft.lgpl.libs.plex/sources/com/kasisoft/lgpl/libs/plex/impl/BooleanTransform.java
@@ -23,11 +23,23 @@ public class BooleanTransform implements ValueTransform {
    * Initializes this transform instance.
    */
   public BooleanTransform() {
-    truevalues = new HashSet<String>();
+    truevalues  = new HashSet<String>();
     truevalues.add( "x"     );
     truevalues.add( "yes"   );
     truevalues.add( "1"     );
     truevalues.add( "true"  );
+  }
+  
+  /**
+   * Sets the values that are considered to represent <code>true</code> values.
+   * 
+   * @param newtrues   The new values representing <code>true</code> values.
+   */
+  public void setTrue( List<String> newtrues ) {
+    truevalues.clear();
+    for( String newtrue : newtrues ) {
+      truevalues.add( newtrue );
+    }
   }
   
   /**
